c95921b80e54bbaf5d2ba3b31c9e3e683a5d79df
Vitor Gomes <zbro2018@gmail.com>
2025-08-22 23:28:07 -0300

    edit: welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,42 @@
-<!-- Padrão -->
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/welcome.css">
+    <title>Flamexs - Loja Virtual</title>
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <nav class="barra-navegacao">
+            <div class="secao-busca">
+                <img src="./images/busca.svg" alt="Buscar">
+                <input type="search" placeholder="O que você está buscando?">
+            </div>
+            
+            <div class="links-navegacao">
+                <a href="#">Início</a>
+                <a href="#">Produtos</a>
+                <a href="#">Sobre nós</a>
+            </div>
+            
+            <div class="secao-logo">
+                <img src="./images/logo.gif" alt="">
+            </div>
+            
+            <div class="links-navegacao">
+                <a href="#">FAQ</a>
+                <a href="#">Trocas e Devoluções</a>
+                <a href="#">Contato</a>
+            </div>
+            
+            <div class="secao-usuario">
+                <a href=""><img src="./images/user.svg" alt=""></a>
+                <a href=""><img src="./images/carrinho.svg" alt=""></a>
+            </div>
+        </nav>
+    </header>
+
+</body>
+</html>
