03ebb9df925bea3023c59176a1e3555afcbc9f28
Allan <Allandesenvolvedor1@gmail.com>
2025-08-24 20:37:35 -0300

    Add pagina contato, sobre e troca

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -1,11 +1,72 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contato - Flamexs</title>
+    <link rel="stylesheet" href="css/contato.css">
+    <link rel="stylesheet" href="css/navbar.css">
 </head>
+
 <body>
-    
+    <header>
+        <nav class="barra-navegacao">
+            <div class="secao-logo">
+                <img src="./images/logo.gif" alt="">
+            </div>
+
+            <div class="links-navegacao">
+                <a href="/">Início</a>
+                <a href="#">Produtos</a>
+                <a href="/sobre">Sobre nós</a>
+                <a href="/faq">FAQ</a>
+                <a href="/troca">Trocas e Devoluções</a>
+                <a href="/contato">Contato</a>
+            </div>
+
+            <div class="secao-usuario">
+                <a href="/login"><img src="./images/user.svg" alt=""></a>
+                <a href=""><img src="./images/carrinho.svg" alt=""></a>
+            </div>
+
+            <button id="botao-menu-mobile" class="botao-menu-mobile">
+                <span class="linha-menu"></span>
+                <span class="linha-menu"></span>
+                <span class="linha-menu"></span>
+            </button>
+        </nav>
+
+        <div id="menu-mobile" class="menu-mobile">
+            <div class="menu-mobile-conteudo">
+                <a href="#">Início</a>
+                <a href="#">Produtos</a>
+                <a href="#">Sobre nós</a>
+                <a href="#">FAQ</a>
+                <a href="#">Trocas e Devoluções</a>
+                <a href="#">Contato</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <h1>Contato</h1>
+        <div class="contato">
+            <p>Atendimento ao Cliente <br>
+                E-commerce: +55 (11) 96935-2617 <br>
+                Loja: +55 (11) 99999-9999 <br>
+                contato@flamescompany.com.br <br>
+                O horário dos nossos canais de atendimento: aplicativo e ligações.
+                Segunda à sexta, das 09:00 às 18:00. <br>
+                Nosso tempo de resposta através de e-mail e Whatsapp é de 24 horas. 
+            </p>
+        </div>
+    </div>
+    <div class="footer">
+
+    </div>
+
+    <script src="js/menu-mobile.js"></script>
 </body>
+
 </html>
--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -1,11 +1,71 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sobre a Flames</title>
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/sobre.css">
 </head>
+
 <body>
-    
+    <header>
+        <nav class="barra-navegacao">
+            <div class="secao-logo">
+                <img src="./images/logo.gif" alt="">
+            </div>
+
+            <div class="links-navegacao">
+                <a href="/">Início</a>
+                <a href="#">Produtos</a>
+                <a href="/sobre">Sobre nós</a>
+                <a href="/faq">FAQ</a>
+                <a href="/troca">Trocas e Devoluções</a>
+                <a href="/contato">Contato</a>
+            </div>
+
+            <div class="secao-usuario">
+                <a href="/login"><img src="./images/user.svg" alt=""></a>
+                <a href=""><img src="./images/carrinho.svg" alt=""></a>
+            </div>
+
+            <button id="botao-menu-mobile" class="botao-menu-mobile">
+                <span class="linha-menu"></span>
+                <span class="linha-menu"></span>
+                <span class="linha-menu"></span>
+            </button>
+        </nav>
+
+        <div id="menu-mobile" class="menu-mobile">
+            <div class="menu-mobile-conteudo">
+                <a href="#">Início</a>
+                <a href="#">Produtos</a>
+                <a href="#">Sobre nós</a>
+                <a href="#">FAQ</a>
+                <a href="#">Trocas e Devoluções</a>
+                <a href="#">Contato</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <h1>SOBRE A FLAMES</h1>
+
+        <P>
+            Surgida em meio à crise de Covid como única esperança de sucesso depois de uma crise familiar, fundada em 2022 <BR></BR>
+            O Time de 5 pessoas da Flames, leva a mensagem de que Flames representa flexibilidade e adaptação, sendo assim, Flames nunca falirá e se adaptará à qualquer crise. <BR></BR>
+            “Não se coloque em uma forma, se adapte e construa sua própria, e deixe a expandir, pode fluir ou colidir. <br>
+            Seja como Flames nos momentos bons e ruins.” <br>
+            Se adapte às circunstâncias ao seu redor. Aprenda a viver o presente, e quando o tempo mudar não seja mais o mesmo do passado. Mude e cresça durante todos os desafios. <br>
+            Trazendo para o vestuário referências asiáticas, transformando o básico e simples com modelagens elaboradas, rompendo fronteiras convencionais, unindo múltiplas estéticas da moda de rua. <br>
+            Flames nega produtos populares convencionais, esperando trazer até do simples, o simples diferente do sistema tradicional. <br>
+            Mostrando que pode mudar e redescobrir o estilo de muitos jovens.
+        </P>
+    </div>
+
+
+    <script src="js/menu-mobile.js"></script>
 </body>
+
 </html>
