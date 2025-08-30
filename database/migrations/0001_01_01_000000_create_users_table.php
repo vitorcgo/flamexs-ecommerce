e478f8516c3f0077d88e42926eb6bf992c6b923c
Gustavo Barros <gustavo.hbarros.sp@gmail.com>
2025-08-30 17:18:28 -0300

    fix: Restore Database
    Alteracoes revertidas por conta de erro

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,24 +12,29 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-
-            //Chaves
             $table->id();
-
-            //Colunas
-            $table->string('full_name');
+            $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('cpf', 14)->unique();
-            $table->string('profile_photo_path', 2048)->nullable();
-            $table->string('phone')->unique();
-            $table->timestamp('last_login_at');
-
-            //Campos de Controle do Laravel
-            $table->rememberToken(); // Para a funcionalidade "Lembrar-me"
+            $table->rememberToken();
             $table->timestamps();
         });
 
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     /**
@@ -38,5 +43,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
