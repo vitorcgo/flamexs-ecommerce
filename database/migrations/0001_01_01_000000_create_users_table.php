779dcdf5937a61c4d9a7814d5c9a9ac7dcb6ce45
Gustavo Barros <gustavo.hbarros.sp@gmail.com>
2025-08-29 23:23:02 -0300

    add: Adicionado todas as models e migrations necessarias para o banco de dados

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,29 +12,23 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+
+            //Chaves Utilizadas na Tabela
             $table->id();
-            $table->string('name');
+            $table->foreignId('address_id')->constrained();
+
+            //Colunas da Tabela User
+            $table->string('full_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('cpf', 14)->unique();
+            $table->string('phone');
+            $table->timestamp('last_access_date');
+
+            //Marca do tempo
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
     }
 
     /**
@@ -43,7 +37,5 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
     }
 };
